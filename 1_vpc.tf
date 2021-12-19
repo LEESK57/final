07495eb0c10a9386fd1d477f7529a3a9cd2124a7
sklee <lsk940507@gmail.com>
2021-12-19 22:19:36 +0900

    sun

--- 1_vpc.tf
+++ 1_vpc.tf
@@ -4,11 +4,11 @@ provider "aws" {
 
 resource "aws_vpc" "final-vpc" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  instance_tenancy     = "default"
   tags = {
     Name                 = "final-vpc"
-    enable_dns_hostnames = true
-    enable_dns_support   = true
-    instance_tenancy     = "default"
   }
 }
 
