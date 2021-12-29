303aa8a52a416f34b7a160b5cafd14b48f025c3e
LEESK57 <94674036+LEESK57@users.noreply.github.com>
2021-12-29 14:24:18 +0900

    Delete web.sh

--- web.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo su -
-yum install -y httpd
-systemctl enable httpd
-systemctl start httpd
-cat >> /var/www/html/index.html << EOF
-<html><body><h1>Hello WEB SERVER  </h1></body></html>
-EOF
